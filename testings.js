2a95d774bb8d402fc1b2c087ffc877b691e8da50
IgnacioGandur <119279723+IgnacioGandur@users.noreply.github.com>
2023-10-05 02:43:27 -0300

    Reverse string finished

--- testings.js
+++ testings.js
@@ -6,7 +6,11 @@ function capitalize(string) {
 }
 
 function reverseString(string) {
-
+    let newString = '';
+    for (let i = string.length - 1; i >= 0; i -= 1) {
+        newString += string[i];
+    }
+    return newString;
 }
 
 module.exports = {
--- testings.test.js
+++ testings.test.js
@@ -4,3 +4,6 @@ const {capitalize, reverseString} = require('./testings');
 test('Capitalize', () => {
     expect(capitalize('sample string')).toBe('Sample string');
 });
+test('Reverse string', () => {
+    expect(reverseString('String reversed.')).toBe('.desrever gnirtS');
+})
