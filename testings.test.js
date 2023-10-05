5f6bf7fec01ebc021ff255ee289b6bc825d7c6e6
IgnacioGandur <119279723+IgnacioGandur@users.noreply.github.com>
2023-10-05 02:53:01 -0300

    Calculator finished

--- testings.test.js
+++ testings.test.js
@@ -1,4 +1,4 @@
-const {capitalize, reverseString} = require('./testings');
+const {capitalize, reverseString, calculator} = require('./testings');
 
 
 test('Capitalize', () => {
@@ -6,4 +6,16 @@ test('Capitalize', () => {
 });
 test('Reverse string', () => {
     expect(reverseString('String reversed.')).toBe('.desrever gnirtS');
+})
+test('Calculator add', () => {
+    expect(calculator.add(5, 5)).toBe(10);
+})
+test('Calculator subtract', () => {
+    expect(calculator.subtract(25, 10)).toBe(15);
+})
+test('Calculator multiply', () => {
+    expect(calculator.multiply(10, 10)).toBe(100);
+})
+test('Calculator divide', () => {
+    expect(calculator.divide(25, 5)).toBe(5);
 })
